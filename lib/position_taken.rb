77062b43b0e7fcf48a124698eddfec65e7ec0f7e
Andres Hernandez <andres.hernandez@utexas.edu>
2020-09-24 22:50:19 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,15 @@
 # code your #position_taken? method here!
+def position_taken?(board, position_taken)
+
+  index = position_taken.to_i - 1
+
+  if board[index] == "   "
+    false
+  elsif board[index] == "  "
+    false
+  elsif board[index] == " "
+    false
+  else
+    true
+    
+end
